4a93dd65f6cfd63d2d2bb3620dc9a2d8438548e8
Lu Jiang <neujianglu@gmail.com>
2026-03-23 23:30:08 +0800

    feat: 增加开机封面

---
+++ app/src/main/java/top/ligoudaner/classpoints/SplashActivity.java
@@ -0,0 +1,25 @@
+package top.ligoudaner.classpoints;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        // 显示 2 秒后跳转到 MainActivity
+        new Handler(Looper.getMainLooper()).postDelayed(() -> {
+            Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }, 2000);
+    }
+}
+
